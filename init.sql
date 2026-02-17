42696b44b999a41157d7d93aad2618e6248640ee
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-02-16 21:56:50 -0500

    Actualizando la BD

--- init.sql
+++ init.sql
@@ -1,10 +1,3 @@
--- 1. Comprobador de existencia de la base de datos
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'MarketDB')
-BEGIN
-    CREATE DATABASE MarketDB;
-END
-GO
-
 USE MarketDB;
 GO
 
